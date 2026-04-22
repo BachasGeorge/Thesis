125908982fc948f69015009fff7e667b3be07674
BachasGeorge <cs02769@uoi.gr>
2026-04-22 19:38:58 +0300

    First Steps

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
 import random
 
-from numpy.f2py.crackfortran import postcrack
-
-
 def calculate_weights(nodes, edges):
 
     weights = []
 
-    for i in range(position):
-        new_weight = nodes[i][0]
-        for j in range(i, position):
-            new_weight += edges[j]
-        weights.append(new_weight)
-
-    for i in range(position,len(nodes)):
-        new_weight = nodes[i][0]
-        for j in range(position,i):
-            new_weight += edges[j]
-        weights.append(new_weight)
-
+    for i in range(len(nodes)):
+        if i < position:
+            travel_cost = sum(edges[i:position])  # edges between i and position
+        elif i > position:
+            travel_cost = sum(edges[position:i])  # edges between position and i
+        else:
+            travel_cost = 0  # current node
+        weights.append(nodes[i][0] - travel_cost)
     return weights
+
 
 def step(nodes, edges):
 
-    nodes[position][1] = 1
-    for j in range(0, len(nodes)):
-        nodes[j][0] -= edges[position-1]
-        if nodes[j][0] < 0 and nodes[j][1] != 1:
-            print(f"Node {j} was not reached before the requested time.")
-            print(nodes)
-            exit()
+    for i in range(0, len(nodes)):
+        if nodes[i][1] == 0:
+            nodes[i][0] -= edges[position - 1]
+            if nodes[i][0] < 0:
+                print(f"Node {i} was not reached before the requested time.")
+                print(nodes)
+                exit()
     return nodes
 
-def new_target(nodes):
+def new_target():
     targets = [x for x in total_weights if nodes[total_weights.index(x)][1] == 0]
     print(targets)
-    if targets == []:
+    if not targets:
         return -1
     target = total_weights.index(min(targets))
     return target
@@ -45,7 +39,7 @@ def new_target(nodes):
 nodes = []
 edges = []
 total_weights = []
-size = 10
+size = 50
 position =  random.randint(0, size-1)
 target = 0
 ALL_VISITED = False
@@ -59,7 +53,7 @@ total_weights = calculate_weights(nodes, edges)
 print(nodes)
 print(total_weights)
 
-target = new_target(nodes)
+target = new_target()
 
 while target != -1:
     print(f"Current position: {position}")
@@ -69,12 +63,15 @@ while target != -1:
             nodes = step(nodes,edges)
             total_weights = calculate_weights(nodes, edges)
             position -= 1
+            nodes[position][1] += 1
         position += 1
     else:
         while position <= target:
             nodes = step(nodes,edges)
             total_weights = calculate_weights(nodes, edges)
             position += 1
+            nodes[position][1] += 1
         position -=1
-    target = new_target(nodes)
+    target = new_target()
     print(nodes)
+print("All nodes were visited.")
